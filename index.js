978838232bb5ca415ac312aebe655e8e3e775584
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2021-02-23 18:31:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu_bar_box").addEventListener('click',navAgain);
+
+function navAgain(){
+    document.querySelector(".navigation").classList.toggle("bring_nav");
+    document.querySelector(".menu").classList.toggle("change");
+}
+
