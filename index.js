915bcd862c856dd9bbbb13620f3e6d3b31a31599
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2021-02-23 18:26:22 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.alert("Hello Dear This website is under Construction"+"<Br>"+"That's why you are loking only front page");
-document.querySelector(".menu_bar_box").addEventListener('click',navAgain);
-
-function navAgain(){
-    document.querySelector(".navigation").classList.toggle("bring_nav");
-    document.querySelector(".menu").classList.toggle("change");
-}
-
