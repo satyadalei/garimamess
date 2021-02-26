8fe29d5b91ffe88eebc10b60e9c327488e02f3cb
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2021-02-26 15:16:38 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".menu_bar_box").addEventListener('click',navAgain);
-
-function navAgain(){
-    document.querySelector(".navigation").classList.toggle("bring_nav");
-    document.querySelector(".menu").classList.toggle("change");
-}
-
